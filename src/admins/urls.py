d4606a30d7538ba7bdc356b71408fe77bdd8998c
hiencao <hiencao0503@gmail.com>
2025-10-22 16:22:52 +0700

    login admin

--- src/admins/urls.py
+++ src/admins/urls.py
@@ -1,9 +1,12 @@
 """ 
 Module xử lý urls.
 """
-from django.urls import path, include
+from django.urls import path
 from . import users
+from . import auth
 
 urlpatterns = [
+    path('login', auth.login_view, name='login'),
+    path('logout', auth.logout_view, name='logout'),
     path('users', users.users, name='users'),
 ]
--- src/testcase_generator/settings/base.py
+++ src/testcase_generator/settings/base.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'admins.middleware.AdminAccessMiddleware',
 ]
 
 ROOT_URLCONF = 'testcase_generator.urls'
